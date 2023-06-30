c896c84dfed1f199344602465cccaaff1bd16727
justalk <justal.kevin@gmail.com>
2023-06-30 21:05:20 +0000

    [CONFIG] Adding the router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import "./App.css";
-import Spline from "@splinetool/react-spline";
-import React, { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Poc1 from "./components/Pocs/Poc1";
 
 function App() {
   return (
-    <div>
-      <div className="projects">
-        <Suspense fallback={<div>Loading...</div>}>
-          <Spline scene="https://prod.spline.design/6Wq1Q7YGyM-iab9i/scene.splinecode" />
-        </Suspense>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/poc1">Poc1</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/poc1" element={<Poc1 />} />
+          <Route path="/" element={<Poc1 />} />
+        </Routes>
       </div>
-      <div className="projects">
-        <Suspense fallback={<div>Loading...</div>}>
-          <Spline scene="https://prod.spline.design/0e-TsmVASNd3Rhym/scene.splinecode" />
-        </Suspense>
-      </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/components/Pocs/Poc1/index.js
@@ -0,0 +1,21 @@
+import Spline from "@splinetool/react-spline";
+import React, { Suspense } from "react";
+
+function Poc1() {
+  return (
+    <div>
+      <div className="projects">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Spline scene="https://prod.spline.design/6Wq1Q7YGyM-iab9i/scene.splinecode" />
+        </Suspense>
+      </div>
+      <div className="projects">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Spline scene="https://prod.spline.design/0e-TsmVASNd3Rhym/scene.splinecode" />
+        </Suspense>
+      </div>
+    </div>
+  );
+}
+
+export default Poc1;
